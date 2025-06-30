e88a785aefa8c9e60c8fe227a98f6aa7f8f6a076
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-30 14:44:38 -0400

    GameInventory.java

--- Update 2 - UML /GameInventory.java
+++ Update 2 - UML /GameInventory.java
@@ -5,12 +5,20 @@ public class GameInventory {
 
         Item sword = database.getItemByName("Sword");
         Item potion = database.getItemByName("Potion");
-
-        playerInventory.addItem(sword);
-        playerInventory.addItem(potion);
-        playerInventory.displayInventory();
+        // Represents the player class that manages or stores game items in the inventory (Does not show in the snippet.)
+        playerInventory.addItem(sword); // Items added sword to the PlayerInventory.
+        playerInventory.addItem(potion); // Items added potion to the PlayerInventory.
+        playerInventory.displayInventory(); // The player's inventory has to be displayed.
 
         playerInventory.removeItem(sword);
         playerInventory.displayInventory();
     }
 }
+/* Important Note: In Update 2 - UML dictionary, all of the class files needed to be separated from the game system to run the compiled file smoothly.
+
+    Named the separate file:
+    - GameInventory.java
+    - Item.java
+    - PlayerInventory.java
+    - StoreInventory.java
+*/
