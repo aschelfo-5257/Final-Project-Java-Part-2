b547abc394728266491bb6910e2168d30f19ca80
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-30 21:26:39 -0400

    GameInventory.java

--- Update 2 - UML /GameInventory.java
+++ Update 2 - UML /GameInventory.java
@@ -7,11 +7,11 @@ public class GameInventory {
         Item sword = database.getItemByName("Sword");
         Item potion = database.getItemByName("Potion");
         // Represents the player class that manages or stores game items in the inventory (Does not show in the snippet.)
-        playerInventory.addItem(sword); // Items added sword to the PlayerInventory.
-        playerInventory.addItem(potion); // Items added potion to the PlayerInventory.
+        if (sword != null) playerInventory.addItem(sword); // Items added sword to the PlayerInventory.
+        if (potion != null) playerInventory.addItem(potion); // Items added potion to the PlayerInventory.
 
-        playerInventory.removeItem(sword); // Removes the “Sword” from the inventory and displays the inventory again.
-        playerInventory.removeItem(potion); // Removes the “Potion” from the inventory and displays the inventory again.
+        if (sword != null) playerInventory.removeItem(sword); // Removes the “Sword” from the inventory and displays the inventory again.
+        if (potion != null) playerInventory.removeItem(potion); // Removes the “Potion” from the inventory and displays the inventory again.
         playerInventory.displayInventory(); // The player's inventory has to be displayed.
     }
 }
