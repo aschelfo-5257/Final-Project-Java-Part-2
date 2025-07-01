738c32daed8cef16dc701b770a5f8b1355fb3e99
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-30 22:49:45 -0400

    ItemDatabase.java

--- Update 2 - UML /ItemDatabase.java
+++ Update 2 - UML /ItemDatabase.java
@@ -3,9 +3,17 @@ import java.util.Map;
 
 public class ItemDatabase {
     private Map<String, Item> items = new ConcurrentHashMap<>();
+
+    public ItemDatabase() {
+        // Example initialization
+        items.put("Sword", new Item("Sword"));
+        items.put("Shield", new Item("Shield"));
     }
 
-    // Main method to make the class runnable
+    public Item getItemByName(String name) {
+        return items.get(name);
+    }
+
     public static void main(String[] args) {
         ItemDatabase db = new ItemDatabase();
         Item item = db.getItemByName("Sword");
