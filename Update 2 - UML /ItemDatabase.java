e4063af6c890366956ed3aeb1ed123c89cd12720
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-30 22:41:06 -0400

    ItemDatabase.java

--- Update 2 - UML /ItemDatabase.java
+++ Update 2 - UML /ItemDatabase.java
@@ -4,4 +4,25 @@ public class ItemDatabase {
         // For demonstration, return a new Item with the given name
         return new Item(name);
     }
+
+    // Main method to make the class runnable
+    public static void main(String[] args) {
+        ItemDatabase db = new ItemDatabase();
+        Item item = db.getItemByName("Sword");
+        System.out.println("Retrieved item: " + item);
+    }
+}
+
+// Sample Item class for demonstration
+class Item {
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
